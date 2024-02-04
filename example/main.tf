d06d523cd0eadeeb5fb4b01222181cc4495340f2
Paul Liljenberg <paul@bane.enterprises>
2024-02-04 09:57:26 +0100

    add example folder

---
+++ example/main.tf
@@ -0,0 +1,23 @@
+
+
+
+module "aws_oidc" {
+  source                = "github.com/coactdev/terraform-aws-oidc-provider"
+  federated_identity_providers = {
+    "gitlab" = {
+      attribute_condition = tolist([
+        {
+          variable = "gitlab:sub"
+          values = [
+            "project_path:coact.dev/infrastructure:ref_type:branch:staging",
+            "project_path:coact.dev/infrastructure:ref_type:branch:production"
+          ]
+        }
+      ])
+      tags       = { "stage" = "bootstrap" }
+      issuer     = "gitlab"
+      policy_arn = ""
+      policy_jsons = data.aws_iam_policy_document.terraform_organisation_management_policy.json
+    }
+  }
+}
